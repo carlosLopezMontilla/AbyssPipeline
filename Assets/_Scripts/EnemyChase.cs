e646a76a5b531c662987f914b63a8229b5e28ebf
Carlos Lopez <carlos.lopez@escuelaestech.es>
2021-02-02 20:32:24 +0100

    Esconderse
    El pez pierde el track del jugador cuando pulsa el shift

--- Assets/_Scripts/EnemyChase.cs
+++ Assets/_Scripts/EnemyChase.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 
 public class EnemyChase : MonoBehaviour
 {
+    public Transform player;
     public Transform other;
+	
 	public float  speed;
+	public float approachSpeed;
+	public float followSpeed;
 	public float vision;
 	public float VisionAngle;
-    
+	
+
+	public PlayerController pContr;
     // Start is called before the first frame update
     void Start()
     {
-        
+		approachSpeed = speed / 2;
+		followSpeed = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-	    transform.position += Vector3.left * speed * Time.deltaTime;
-        
-        Vector3 displacement = other.position - transform.position;
+		transform.position += Vector3.left * speed * Time.deltaTime;
+		
+		Vector3 displacement = player.position - transform.position;
 
 	    float range = displacement.magnitude;
         float EnemyVision = VisionAngle / 2;
 	    float EnemyAngle = Vector3.Angle(Vector3.left, displacement);
-        
-	    transform.position += -transform.right * speed * Time.deltaTime;
-	    if (range < vision && range < vision && EnemyVision > EnemyAngle  )
-	        transform.position = Vector3.MoveTowards(transform.position, other.position, speed * Time.deltaTime);
-            
-    }
+
+
+		if (pContr.isCrouched == true)
+			transform.position += Vector3.left * 0 *Time.deltaTime;
+
+		if (pContr.isCrouched == false && range < vision && range < vision && EnemyVision > EnemyAngle)
+				transform.position = Vector3.MoveTowards(transform.position, player.position, approachSpeed * Time.deltaTime);
+		
+	}
 	// Implement OnDrawGizmos if you want to draw gizmos that are also pickable and always drawn.
 	protected void OnDrawGizmos()
 	{
@@ -39,11 +48,9 @@ public class EnemyChase : MonoBehaviour
 		Gizmos.DrawWireSphere(transform.position,vision);
 		
 		Gizmos.color = Color.red;
-		Gizmos.DrawLine(other.position, transform.position);
-		
-		
-		
+		Gizmos.DrawLine(player.position, transform.position);
+
 		
 	}
-
+	
 }
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿                using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -22,9 +22,11 @@ public class PlayerController : MonoBehaviour
     private bool jumpNow = false;
     public int jumpLimit;
     public float gravity;
-
     public bool isGrounded;
     bool jumpPressed;
+
+    [Header("Crouch")]
+    public bool isCrouched;
 
    
     
@@ -33,6 +35,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         isGrounded = true;
 	    jumpPressed = false;
+        isCrouched = false;
 	    speed = OriginalSpeed;
 	    initialPower = chargePower;
     
@@ -41,6 +44,16 @@ public class PlayerController : MonoBehaviour
     {
         Ray ray = new Ray(transform.position, Vector3.down);
         isGrounded = Physics.Raycast(ray, 1.02f, LayerMask.GetMask("Ground"));
+
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            isCrouched = true;
+        }else
+        {
+            isCrouched = false;
+        }
+       
+
 
         if (isGrounded && Input.GetKey(KeyCode.Space))
         {
@@ -52,7 +65,7 @@ public class PlayerController : MonoBehaviour
 	        
             
         }
-        if(Input.GetKeyUp(KeyCode.Space))
+        if(Input.GetKeyUp(KeyCode.Space) && isCrouched == false)
         {
             jumpNow = true;
         }
@@ -62,19 +75,23 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
 	    moveInput = Input.GetAxisRaw("Horizontal");
-	    if(moveInput != 0 )
-	    {
-		    speed+=acceleration;
-		    if(speed > maxSpeed)
-		    {
-			    speed = maxSpeed;
-		    }
-	    }
-	    else
-	    {
-		    speed = OriginalSpeed;
-	    }
-        rb.velocity = new Vector3(moveInput * speed, rb.velocity.y);
+        
+            if (moveInput != 0)
+            {
+                speed += acceleration;
+                if (speed > maxSpeed)
+                {
+                    speed = maxSpeed;
+                }
+            }
+            else
+            {
+                speed = OriginalSpeed;
+            }
+        if (isCrouched == false)
+        {
+            rb.velocity = new Vector3(moveInput * speed, rb.velocity.y);
+        }
         if (jumpNow)
         {
             holdToJump();
@@ -89,6 +106,7 @@ public class PlayerController : MonoBehaviour
 	    chargePower = initialPower;
     }
     
+  
 
 
 
