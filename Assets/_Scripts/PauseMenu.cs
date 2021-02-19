6a93ffe47d37d1d9756ee34bf27b0aafa2ebe575
Monty014 <77298181+Monty014@users.noreply.github.com>
2021-02-19 11:47:20 +0100

    Pause Menu + Enemy
    Implementacion de las mecanicas del pez y el menu de pausa

---
+++ Assets/_Scripts/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
