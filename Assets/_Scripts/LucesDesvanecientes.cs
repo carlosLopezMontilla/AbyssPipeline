69fa940cacd4e5acc02f66147e95fe7b2f8c652c
Shenono <77298168+Shenono@users.noreply.github.com>
2021-05-05 15:10:00 +0200

    Luces desvaneciente
    Entras en un trigger y se desvance la luz que vincules a myLight con la velocidad que indiques en multiplicar.

---
+++ Assets/_Scripts/LucesDesvanecientes.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LucesDesvanecientes : MonoBehaviour
+	{
+		public Light myLight;
+		public float OriginalIntensity;
+		public bool Dentro;
+		public float multiplicar;
+		void Start()
+		{
+			
+			OriginalIntensity= myLight.intensity;
+		}
+
+		void Update()
+		{
+			if(Dentro)
+			{
+				IntensityDown();
+			}
+			if (!Dentro)
+			{
+				RestoreIntensity();
+			}
+		}
+		// OnTriggerEnter is called when the Collider other enters the trigger.
+		private void OnTriggerEnter(Collider other)
+		{
+			Dentro = true;
+		}
+		// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+		private void OnTriggerExit(Collider other)
+		{
+			Dentro = false;
+		}
+		void IntensityDown()
+		{
+			myLight.intensity = (myLight.intensity - multiplicar * Time.deltaTime );
+			if (myLight.intensity < 0)
+			{
+				myLight.intensity = 0;
+			}
+		}
+		void RestoreIntensity()
+		{
+			
+			myLight.intensity = OriginalIntensity;
+		}
+	}
+
