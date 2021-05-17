ce0c5962d11721d7c5702aa74b4749bbd7464106
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-05-17 22:49:56 +0200

    PrototypeSceneFinished

---
+++ Assets/_Scripts/Teleporter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+	public Transform tpTarget;
+	public GameObject Player;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		Player.transform.position = tpTarget.transform.position;		
+	}
+}
