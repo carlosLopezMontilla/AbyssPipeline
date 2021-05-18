1df67b30ce05b5177099029b9c82f6e4213e707b
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-05-18 20:29:45 +0200

    Script CameraZoomFix
    Now moving in Z not the FOV

---
+++ Assets/_NewLevel_Prefabs/CameraMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+	public bool zoomActive = false;
+	public Vector3[] Target;
+	public GameObject Cam;
+	public float Speed;
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+			zoomActive = true; 
+	}
+	
+	void OnTriggerExit(Collider other)
+	{
+		if(other.gameObject.CompareTag("Player"))
+			zoomActive = false;
+	}
+	
+	public void Update()
+	{
+		if(zoomActive){
+			Cam.transform.position = Vector3.Lerp(Cam.transform.position,Target[1],Speed*Time.deltaTime);
+		}
+		else{
+			Cam.transform.position = Vector3.Lerp(Cam.transform.position,Target[0],Speed*Time.deltaTime);			
+		}
+
+
+	}
+}
