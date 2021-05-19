4ff2608291c6df128a7c418da2f9294c527bbbf5
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-05-19 17:57:10 +0200

    CameraZoomRemove
    ZoomNotWorking Fix

--- Assets/_NewLevel_Prefabs/CameraMove.cs
+++ Assets/_NewLevel_Prefabs/CameraMove.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class CameraMove : MonoBehaviour
 {
 	public bool zoomActive = false;
-	public Vector3[] Target;
-	public GameObject Cam;
+	public float distance;
+	public Transform Cam;
 	public float Speed;
 	
 	void OnTriggerEnter(Collider other)
@@ -21,15 +21,18 @@ public class CameraMove : MonoBehaviour
 			zoomActive = false;
 	}
 	
+	
 	public void Update()
 	{
+		Vector3 CamPosition = new Vector3(Cam.position.x, Cam.position.y, Cam.position.z);
+		
 		if(zoomActive){
-			Cam.transform.position = Vector3.Lerp(Cam.transform.position,Target[1],Speed*Time.deltaTime);
+			Cam.transform.position = Vector3.Lerp(CamPosition, CamPosition + new Vector3 (0,0,distance), Speed * Time.deltaTime);
+
 		}
 		else{
-			Cam.transform.position = Vector3.Lerp(Cam.transform.position,Target[0],Speed*Time.deltaTime);			
+			Cam.transform.position = Vector3.Lerp(CamPosition, CamPosition + new Vector3 (0,0,-distance), Speed * Time.deltaTime);
+
 		}
-
-
 	}
 }
---
+++ Assets/_NewLevel_Prefabs/NewCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewCameraFollow : MonoBehaviour
+{
+	public Transform player;
+	public float x;
+	public float y;
+	public float z;
+
+	void Update()
+	{
+
+
+		transform.position = new Vector3(player.position.x, player.position.y, player.position.z) + new Vector3(x,y,z);
+                
+		
+	}
+}
