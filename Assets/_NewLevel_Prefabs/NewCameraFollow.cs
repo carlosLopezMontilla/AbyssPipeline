7fd3c1add5920dc88c43cdd2b0c0615b0e61cc0f
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-06-07 21:52:53 +0200

    PrepEscena + Iluminacion
    Escena in progress + Luz

--- Assets/_NewLevel_Prefabs/NewCameraFollow.cs
+++ Assets/_NewLevel_Prefabs/NewCameraFollow.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class NewCameraFollow : MonoBehaviour
 {
-	public CameraZoomNew zoom;
-	public Transform player;
-
-	public void Update()
-	{
-
-		transform.position = new Vector3(player.position.x, player.position.y, player.position.z) + new Vector3(zoom.x,zoom.y,zoom.z);
-                
-	}
-	
-	
+    public Transform player;
+    public Vector3 highPosition;
+    void Update()
+    {
+        transform.position = player.position + highPosition;
+    }
 }
+
