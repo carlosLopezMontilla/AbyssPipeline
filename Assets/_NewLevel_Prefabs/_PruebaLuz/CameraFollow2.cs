e56618add69247f89516c8c2cf763dadac0e4313
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-06-08 22:34:30 +0200

    SceneDeco v1
    Deco in the scene

---
+++ Assets/_NewLevel_Prefabs/_PruebaLuz/CameraFollow2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow2 : MonoBehaviour
+{
+    public bool zoomActive;
+    public Vector3[] Target;
+    public Transform player;
+    public float speed;
+    public Transform cam;
+    public Vector3 camMove;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            zoomActive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            zoomActive = false;
+        }
+    }
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+
+        if (zoomActive)
+        {
+            //cam.transform.position = player.transform.position + Vector3.Lerp(Target[1], Target[0], speed * Time.deltaTime);
+            camMove = Vector3.MoveTowards(Target[1], Target[0], step);
+        }
+        else
+        {
+            //cam.transform.position = player.transform.position + Vector3.Lerp(Target[0], Target[1], speed * Time.deltaTime);
+            camMove = Vector3.MoveTowards(Target[0], Target[1], step);
+
+        }
+
+        cam.transform.position = player.transform.position + camMove;
+    }
+}
---
+++ Assets/_NewLevel_Prefabs/_PruebaLuz/CameraZoom2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom2 : MonoBehaviour
+{
+    public CameraFollow2 isZoom;
+    public Vector3 zoomPos;
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isZoom.zoomActive = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other) 
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isZoom.zoomActive = false;
+        }
+    }
+}
