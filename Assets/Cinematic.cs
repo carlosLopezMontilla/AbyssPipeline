dfd7834a3344f13878841f28ef4f9bb2107ce8fb
Javier-FP <77298163+Javier-FP@users.noreply.github.com>
2021-06-10 22:16:47 +0200

    SceneEnd
    End of decoration

--- Assets/Cinematic.cs
+++ Assets/Cinematic.cs
@@ -7,6 +7,9 @@ public class Cinematic : MonoBehaviour
 {
 	public PlayableDirector timeline;
 	public Rigidbody player;
+	public float timer;
+	public Jump noJump;
+	public GameObject HUD;
 	
     // Start is called before the first frame update
 
@@ -21,8 +24,13 @@ public class Cinematic : MonoBehaviour
 
 	IEnumerator noMove()
 	{
-		player.constraints = RigidbodyConstraints.FreezeAll;
-		yield return new WaitForSeconds(7.5f);
+		player.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezeRotation;
+		noJump.enabled = false;
+		HUD.SetActive(false);
+		yield return new WaitForSeconds(timer);
 		player.constraints = RigidbodyConstraints.FreezeRotation;
+		noJump.enabled = true;
+		HUD.SetActive(true);
+		Destroy(this);
 	}
 }
